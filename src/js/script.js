ec38b79062c6fca2834b32d83df9598231303c01
Yevhenii <eygehavv27@gmail.com>
2023-08-01 13:20:52 +0300

    refactor: change script.js for working with NoteService and DOMHelper

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+import { NoteService } from './NoteService.js'
+import { DOMHelper, popups } from './DOMHelper.js';
+import configureWindowEvents from './configureWindowEvents.js';
+
+const noteService = new NoteService();
+const domHelper = new DOMHelper();
+
+window.localStorage.setItem('interactionNoteId', '');
+window.localStorage.setItem('removeNoteId', '');
+window.localStorage.setItem('archiveNoteId', '');
+
+noteService.notes.forEach(note => {
+    domHelper.addNote(note);
+})
+configureWindowEvents(noteService, domHelper);
+
+
+domHelper.updateNoteStatistics(noteService.notes, noteService.archivedNotes);
+
+console.log(noteService.getNotes());
